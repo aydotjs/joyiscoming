0cfe4d35bf5dce03115a4237d9136b764b2fc7d7
aydotjs <ayobami66@gmail.com>
2026-02-02 23:21:19 +0100

    o

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['swiper'],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
 };
-
 export default nextConfig;
